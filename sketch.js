aa017abff6538cee3172fd23d98872e8972953db
carlwallin <40901056+carlwallin@users.noreply.github.com>
2023-12-23 13:33:29 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,15 @@
-// Symmetry corresponding to the number of reflections. Change the number for different number of reflections
-let symmetry = 6;
-
-let angle = 360 / symmetry;
+let symmetry;
+let angle;
+let angleB = 0;
 let saveButton, clearButton, mouseButton, keyboardButton;
-let slider;
-
+let symmetrySlider, symSizeSlider;
+let brushSizeSlider, sizeSlider;
+let brushOpSlider, sizeOpSlider;
 let canvas;
 
 function setup() {
-  //createCanvas(710, 710);
-  //background(255);
-
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  // canvas.style('z-index', '-1');
-
   angleMode(DEGREES);
 
   // Creating the save button for the file
@@ -29,48 +24,52 @@ function setup() {
   clearButton.position(60, 10);
   clearButton.class("tryckknapp");
 
-  // Creating the button for Full Screen
-  fullscreenButton = createButton("Full Screen");
-  fullscreenButton.mousePressed(screenFull);
-  fullscreenButton.position(110, 10);
-  fullscreenButton.class("tryckknapp");
+  // Setting up the slider for symmetry
+  symmetrySlider = createButton("Symmetry Slider");
+  symmetrySlider.position(10, 60);
+  symmetrySlider.class("textknapp");
+  symSizeSlider = createSlider(4, 32, 12, 1); // Startvärde 12
+  symSizeSlider.position(140, 60);
+  symSizeSlider.size(200);
+  symSizeSlider.class("slider");
 
   // Setting up the slider for the thickness of the brush
   brushSizeSlider = createButton("Brush Size Slider");
-  brushSizeSlider.position(10, 35);
+  brushSizeSlider.position(10, 90);
   brushSizeSlider.class("textknapp");
-  sizeSlider = createSlider(0.1, 200, 10, 0.1);
-  sizeSlider.position(140, 35);
+  sizeSlider = createSlider(0.1, 100, 10, 0.1);
+  sizeSlider.position(140, 90);
   sizeSlider.size(200);
   sizeSlider.class("slider");
 
   brushOpSlider = createButton("Brush Opacity Slider");
-  brushOpSlider.position(10, 55);
+  brushOpSlider.position(10, 120);
   brushOpSlider.class("textknapp");
-  sizeOpSlider = createSlider(1, 100, 100, 0.1);
-  sizeOpSlider.position(140, 55);
+  sizeOpSlider = createSlider(1, 255, 100, 0.1);
+  sizeOpSlider.position(140, 120);
   sizeOpSlider.size(200);
   sizeOpSlider.class("slider");
 }
 
-// Save File Function
 function saveFile() {
   save("design.jpg");
 }
 
-// Clear Screen function
 function clearScreen() {
   background(255);
 }
 
-// Full Screen Function
-function screenFull() {
-  let fs = fullscreen();
-  fullscreen(!fs);
-}
-
 function draw() {
   translate(width / 2, height / 2);
+
+  // Uppdatera symmetry och angle baserat på skjutreglaget
+  symmetry = symSizeSlider.value();
+  angle = 360 / symmetry;
+
+  let sx = map(sin(angleB), -1, 1, 0.1, 80);
+  let ox = map(sin(angleB), -1, 1, 150, 1);
+  sizeSlider.value(sx);
+  sizeOpSlider.value(ox);
 
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
     let mx = mouseX - width / 2;
@@ -93,4 +92,5 @@ function draw() {
       }
     }
   }
+  angleB += 1;
 }
